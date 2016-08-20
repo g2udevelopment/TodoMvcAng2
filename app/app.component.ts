470b415697dfd002f97a4712f2e0fc6153f3e9e9
Gerben van de Wiel <cgw@bakkerbarendrecht.nl>
2016-08-20 14:16:21 +0200

    component-before-ngModel

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,63 @@
 import { Component } from '@angular/core';
+import { Todo } from './model/store';
 
 @Component({
   selector:'todo-app',
-  template: '<h1>Todo app setup</h1>'
+  template: `<input placeholder="Wat moet er gebeuren?" autofocus="" [(ngModel)]="newTodoText">
+              <ul class="todo-list">
+              <li *ngFor="#todo of todoStore">
+              <span>{{todo.title}}</span>
+              </li>
+            </ul>
+  `
 })
-export class AppComponent {}
+export class AppComponent {
+  todoStore: Array<Todo>;
+  newTodoText = '';
+
+  constructor() {
+
+  }
+
+  stopEditing(todo: Todo, editedTitle: string) {
+    todo.title = editedTitle;
+    todo.editing = false;
+  }
+
+  cancelEditingTodo(todo: Todo) {
+    todo.editing = false;
+  }
+
+  updateEditingTodo(todo: Todo, editedTitle: string) {
+    editedTitle = editedTitle.trim();
+    todo.editing = false;
+
+    if (editedTitle.length === 0) {
+      return this.todoStore.splice(this.todoStore.indexOf(todo),1);
+    }
+
+    todo.title = editedTitle;
+  }
+
+  editTodo(todo: Todo) {
+    todo.editing = true;
+  }
+
+  removeCompleted() {
+  }
+
+  toggleCompletion(todo: Todo) {
+	}
+
+	remove(todo: Todo){
+	}
+
+  addTodo() {
+    if (this.newTodoText.trim().length) {
+      this.todoStore.push(new Todo(this.newTodoText));
+      this.newTodoText = '';
+    }
+  }
+
+
+}
